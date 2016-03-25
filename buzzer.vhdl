9ea57732b298fdd3fcbb5bfe1c96fb12916a5629
hoommaah <hcmalijan@up.edu.ph>
2016-03-25 21:15:31 +0800

    comment and fix formula

--- buzzer.vhdl
+++ buzzer.vhdl
@@ -1,8 +1,15 @@
+--Homer Malijan
+--Gabrielle Pauline Payawal
+--T-2L
+--VHDL programming assignment
+
 -- Library Statements
 library IEEE; use IEEE.std_logic_1164.all;
 
 -- Entity Definition
 entity buzzer is
+	-- 1 = out
+	-- 0 = in
 	port (
 		valid: out std_logic;
 		boggis0: in std_logic;
@@ -16,6 +23,7 @@ end entity buzzer;
 
 architecture buzzer of buzzer is
 begin
-	valid <= (boggie0 or boggie1)or(bunce0 or bunce1)or(bins0 or bins1);
+	--valid when atleast one from any of in and out is on
+	valid <= (boggis0 or bunce0 or bean0) and (boggis1 or bunce1 or bean1);
 end architecture buzzer;
 
